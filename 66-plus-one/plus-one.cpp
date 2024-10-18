be8454b8f1c122a5c328aacc55c638469c5d72c9
Nishant singh <114793917+nishantsingh1107@users.noreply.github.com>
2024-10-18 22:50:17 +0530

    Time: 0 ms (100.00%) | Memory: 10.7 MB (47.26%) - LeetSync

--- 66-plus-one/plus-one.cpp
+++ 66-plus-one/plus-one.cpp
@@ -6,20 +6,15 @@ public:
     vector<int> plusOne(vector<int>& digits) {
         int n = digits.size();
 
-        // Traverse the digits from the last element to the first
         for (int i = n - 1; i >= 0; i--) {
             if (digits[i] < 9) {
-                // If the digit is less than 9, just increment it and return the result
                 digits[i] += 1;
                 return digits;
             }
-            // If the digit is 9, set it to 0 (since 9 + 1 = 10, we carry the 1 to the next digit)
+            
             digits[i] = 0;
         }
-
-        // If all digits are 9, we need to add a new 1 at the beginning
         digits.insert(digits.begin(), 1);
-        
         return digits;
     }
 };
