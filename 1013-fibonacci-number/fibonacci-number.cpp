40013c2d62e5a8010b3c8952c19130d4ba0bf84e
Nishant singh <114793917+nishantsingh1107@users.noreply.github.com>
2024-10-19 23:35:22 +0530

    Time: 2 ms (80.00%) | Memory: 7.7 MB (12.40%) - LeetSync

---
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -0,0 +1,18 @@
+#include <vector>
+
+class Solution {
+public:
+    int fib(int n) {
+        if (n == 0) return 0;
+        if (n == 1) return 1;
+
+        std::vector<int> arr(n);
+        arr[0] = 0;
+        arr[1] = 1;
+        for (int i = 2; i < n; i++) {
+            arr[i] = arr[i - 1] + arr[i - 2];
+        }
+        return arr[n - 1] + arr[n - 2];
+    }
+};
+
