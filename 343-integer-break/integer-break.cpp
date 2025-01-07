71029f59c9d2233707ce570a99adf45641634125
Nishant singh <114793917+nishantsingh1107@users.noreply.github.com>
2025-01-07 18:26:12 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (71.11%) - LeetSync

---
+++ 343-integer-break/integer-break.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int integerBreak(int n) {
+        if(n == 2) return 1;
+        if(n == 3) return 2;
+        int a = 1;
+        while(n>4){
+            a *= 3;
+            n -= 3;
+        }
+        a *= n;
+        return a;
+    }
+};
