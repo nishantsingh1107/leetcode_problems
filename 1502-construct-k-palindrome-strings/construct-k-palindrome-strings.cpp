ad22feb8b6d75b578a54d4afe9fb48b5fef61d94
Nishant singh <114793917+nishantsingh1107@users.noreply.github.com>
2025-01-12 18:36:23 +0530

    Time: 25 ms (12.00%) | Memory: 14.9 MB (67.38%) - LeetSync

---
+++ 1502-construct-k-palindrome-strings/construct-k-palindrome-strings.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool canConstruct(string s, int k) {
+        if(s.length()<k) return false;
+        sort(s.begin(), s.end());
+        int odd_cnt = 0;
+        for(int i=0;i<s.length();){
+            char curr = s[i];
+            int cnt = 0;
+            while(i<s.length() && s[i] == curr){
+                cnt++;
+                i++;
+            }
+            if(cnt%2 != 0) odd_cnt++;
+        }
+        return odd_cnt<=k;
+    } 
+};
