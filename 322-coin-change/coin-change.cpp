34b708ffe1ebd93c0e90ec1a1433b2e4ec821dce
Nishant singh <114793917+nishantsingh1107@users.noreply.github.com>
2025-03-11 10:46:27 +0530

    Time: 15 ms (92.26%) | Memory: 17.8 MB (80.72%) - LeetSync

---
+++ 322-coin-change/coin-change.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int coinChange(vector<int>& coins, int amount) {
+        vector<int> dp(amount+1, 1e9);
+        dp[0] = 0;
+        for(int i=1;i<=amount;i++){
+            for(auto coin : coins){
+                if(i >= coin){
+                    dp[i] = min(dp[i], 1+dp[i-coin]);
+                }
+            }
+        }
+        if(dp[amount] == 1e9) return -1;
+        return dp[amount];
+    }
+};
